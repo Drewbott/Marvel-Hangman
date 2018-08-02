2bd1c34ff40175ab1caa156443686322a3b713bd
Andrew Norris <andrewnorris@Andrews-MacBook-Air.local>
2018-08-02 07:18:21 -0700

    Version 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,8 +8,6 @@
 // document.write (yourName)
 
 
-// Psuedo Code to ddescribe what the game needs to do
-
 // Pick a random Word
 var words = [
 "wolverive",
@@ -28,6 +26,7 @@ var words = [
 "redskull",
 ];
 
+var win = 0
 var word = words[Math.floor(Math.random() * words.length)];
 
 // While the word has not yet been guessed {
@@ -39,35 +38,58 @@ var word = words[Math.floor(Math.random() * words.length)];
 
     var remainingLetters = word.length;
 
-    while (remainingLetters > 0) {
+    // while (remainingLetters > 0) {
     // Show the player their current progress
-    alert(answerArray.join(" "));
+    var puzzle = document.getElementById("puzzle");
+
+    // puzzle.textContent = answerArray.join(" ");
+    // alert(answerArray.join(" "));
   
     //  Get a guess from the player
 
-    var guess = prompt("Guess a letter, or click cancel to quit.");
+    document.onkeyup = function(event) {
+        var guess = event.key;
+        // if (guess === null) {
+        //     break;
+        // } else if (guess.length !== 1) {
+        //     alert("Please enter a single letter.");
+        // }else {
+            for (var j=0; j < word.length; j++){
+                if (word[j] === guess) {
+                    answerArray[j] = guess;
+                    remainingLetters--;
+                }
+                }
+            }
+        // }
+        puzzle.textContent = answerArray.join(" ");
+    
+// Create a counter that counts up when the word is guessed
+    
+
+    // var guess = prompt("Guess a letter, or click cancel to quit.");
 
     // If the player wants to quit the game {
-        if (guess === null) {
-            break;
-        } else if (guess.length !== 1) {
-            alert("Please enter a single letter.");
-        }
+        // if (guess === null) {
+        //     break;
+        // } else if (guess.length !== 1) {
+        //     alert("Please enter a single letter.");
+        // }
 
 // Else {
 // If the player's guess is the word
 // Update the player with the guess
-else {
-    for (var j=0; j < word.length; j++){
-        if (word[j] === guess) {
-            answerArray[j] = guess;
-            remainingLetters--;
-        }
-    }
-}
-    }
+// else {
+//     for (var j=0; j < word.length; j++){
+//         if (word[j] === guess) {
+//             answerArray[j] = guess;
+//             remainingLetters--;
+//         }
+//     }
+// }
+//     }
 // Congratulate the player
-alert(answerArray.join(" "));
-alert("Excelsior! The answer was " + word);
+// alert(answerArray.join(" "));
+// alert("Excelsior! The answer was " + word);
 
  
